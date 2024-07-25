edb1da7e04e4aa30c0b90930670f1614fe3ed77f
ramesh <ramesh.pamisetty01@gmail.com>
2024-07-25 18:56:04 +0530

    First Commit

---
+++ LoggingApps/src/main/java/com/ramesh/LoggingAppsApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingAppsApplication.class, args);
+	}
+
+}
